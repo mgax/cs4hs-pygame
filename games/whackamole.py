9513f6598c7c12c3b0aae708f68796899fb1282a
Alex Eftimie <alex@eftimie.ro>
2014-10-22 13:07:55 +0300

    Score

--- games/whackamole.py
+++ games/whackamole.py
@@ -3,11 +3,15 @@ import pygame
 
 
 def main():
+    pygame.init() # for font
     screen_rect = pygame.Rect((0, 0), (640, 480))
     screen = pygame.display.set_mode(screen_rect.size)
     mole = pygame.Rect((0, 0), (40, 40))
     whack = pygame.Rect((0, 0), (10, 10))
     color = pygame.Color('#4444ff')
+    myfont = pygame.font.SysFont("monospace", 25)
+    hit = 0
+    miss = 0
 
     while True:
         for event in pygame.event.get():
@@ -21,11 +25,19 @@ def main():
                     x, y = random.randint(0, 640), random.randint(0, 480)		
                     mole.center = (x, y)
                     mole.clamp_ip(screen_rect)
+                    hit = hit + 1
+                else:
+                    miss = miss + 1
 
+        # draw mole
         screen.fill(pygame.Color('#ffffff'))
         pygame.draw.circle(screen, color, mole.center, 20)
+        # draw score
+        score = "Hit: {} Miss: {}".format(hit, miss)
+        label = myfont.render(score, 1, color)
+        screen.blit(label, (200, 0))
+        
         pygame.display.flip()
-
         pygame.time.wait(50)
 
 
