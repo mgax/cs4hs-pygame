2478970ea798da75103843ec1e679c00031b6e22
Alex Morega <alex@grep.ro>
2014-11-07 23:33:18 +0200

    vy

--- game2014.py
+++ game2014.py
@@ -5,6 +5,7 @@ def main():
     R = 30
     x = 0
     vx = 10
+    vy = 10
     world = pygame.Rect((0, 0), (800, 600))
     ball = pygame.Rect(world.center, (2*R, 2*R))
 
@@ -12,10 +13,12 @@ def main():
     screen = pygame.display.set_mode(world.size)
 
     while True:
-        ball.move_ip(vx, 0)
+        ball.move_ip(vx, vy)
         ball.clamp_ip(world)
         if ball.right == world.right or ball.left == world.left:
             vx = -vx
+        if ball.top == world.top or ball.bottom == world.bottom:
+            vy = -vy
 
         screen.fill(pygame.Color('white'))
         pygame.draw.circle(screen, pygame.Color('blue'), ball.center, R)
