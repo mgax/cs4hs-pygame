0e988cf820621407a667f0700d080407634483bb
Alex Morega <alex@grep.ro>
2014-11-07 23:04:40 +0200

    moving ball

--- game2014.py
+++ game2014.py
@@ -2,11 +2,15 @@ import pygame
 
 
 def main():
+    x = 0
+
     screen = pygame.display.set_mode((800, 600))
-    screen.fill(pygame.Color('white'))
-    pygame.draw.circle(screen, pygame.Color('blue'), (400, 300), 30)
-    pygame.display.flip()
-    pygame.time.wait(1000)
+
+    while True:
+        x += 10
+        screen.fill(pygame.Color('white'))
+        pygame.draw.circle(screen, pygame.Color('blue'), (x, 300), 30)
+        pygame.display.flip()
 
 
 main()
