f47828a31954c1f4f621d0c76bc695b1aafc4c99
Alex Morega <alex@grep.ro>
2014-11-07 23:28:42 +0200

    use rects for calculation

--- game2014.py
+++ game2014.py
@@ -4,21 +4,20 @@ import pygame
 def main():
     x = 0
     vx = 10
+    world = pygame.Rect((0, 0), (800, 600))
+    ball = pygame.Rect(world.center, (60, 60))
 
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((800, 600))
 
     while True:
-        x += vx
-        if x > 800:
-            x = 800
-            vx = -vx
-        if x < 0:
-            x = 0
+        ball.move_ip(vx, 0)
+        ball.clamp_ip(world)
+        if ball.right == world.right or ball.left == world.left:
             vx = -vx
 
         screen.fill(pygame.Color('white'))
-        pygame.draw.circle(screen, pygame.Color('blue'), (x, 300), 30)
+        pygame.draw.circle(screen, pygame.Color('blue'), ball.center, 30)
         pygame.display.flip()
         clock.tick(30)
 
