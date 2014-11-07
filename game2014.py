5f393a0255904a5003e264b590cf9df3c45afea3
Alex Morega <alex@grep.ro>
2014-11-07 23:18:41 +0200

    bounce at horizontal edges

--- game2014.py
+++ game2014.py
@@ -3,14 +3,19 @@ import pygame
 
 def main():
     x = 0
+    vx = 10
 
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((800, 600))
 
     while True:
-        x += 10
+        x += vx
         if x > 800:
             x = 800
+            vx = -vx
+        if x < 0:
+            x = 0
+            vx = -vx
 
         screen.fill(pygame.Color('white'))
         pygame.draw.circle(screen, pygame.Color('blue'), (x, 300), 30)
