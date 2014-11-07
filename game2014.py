765b6362addac5e2b8223e0563d8c9817d7d8325
Alex Morega <alex@grep.ro>
2014-11-07 23:46:17 +0200

    place food on screen

--- game2014.py
+++ game2014.py
@@ -3,11 +3,13 @@ import pygame
 
 def main():
     R = 30
+    FOOD_R = 10
     x = 0
-    vx = 0
-    vy = 0
+    vx = 10
+    vy = 10
     world = pygame.Rect((0, 0), (800, 600))
     ball = pygame.Rect(world.center, (2*R, 2*R))
+    food = None
 
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode(world.size)
@@ -17,6 +19,10 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     return
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                food = pygame.Rect((0, 0), (FOOD_R*2, FOOD_R*2))
+                food.center = event.pos
 
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_DOWN]:
@@ -37,6 +43,8 @@ def main():
 
         screen.fill(pygame.Color('white'))
         pygame.draw.circle(screen, pygame.Color('blue'), ball.center, R)
+        if food:
+            pygame.draw.circle(screen, pygame.Color('red'), food.center, FOOD_R)
         pygame.display.flip()
         clock.tick(30)
 
