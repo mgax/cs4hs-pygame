08054138125e104f02dabd11e1c24da9ad7c69be
Alex Morega <alex@grep.ro>
2014-11-07 22:47:58 +0200

    draw circle

--- game2014.py
+++ game2014.py
@@ -4,6 +4,7 @@ import pygame
 def main():
     screen = pygame.display.set_mode((800, 600))
     screen.fill(pygame.Color('white'))
+    pygame.draw.circle(screen, pygame.Color('blue'), (400, 300), 30)
     pygame.display.flip()
     pygame.time.wait(1000)
 
